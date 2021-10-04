f5bb651603971bd54e5abe2e758871ea1388e259
VadimKa <vadimmm120@yandex.ru>
2021-10-04 23:12:22 +0300

    added separation between systems
    windows has two methods. First windows method in progress.

--- Pr3/shredder/main.py
+++ Pr3/shredder/main.py
@@ -1,13 +1,20 @@
 import os
+import random
+import sys
+
+import pytest
 
 PATH = os.path.dirname(__file__)
-FILE_NAME = 'testfile.txt'
+FILE_NAME = 'testfile'
 FILE_PATH = os.path.join(PATH, FILE_NAME)
 FILE_DATA = 'Credentials\n' \
             'Email: somemail@gmail.com\n' \
             'Password: secret\n' \
             'Bank card: 13214564797\n' \
-            'CVV: 456'
+            'CVV: 456\n' \
+            'Other important private information\n'
+
+IS_WIN = sys.platform.startswith('win')
 
 
 class TestCreator:
@@ -19,25 +26,34 @@ class TestCreator:
 
     def test_fill_file(self):
         file = open(FILE_NAME, 'w+')
-        file.write(FILE_DATA)
+        file.write(FILE_DATA * 10)
         file.close()
         file = open(FILE_NAME, 'r')
-        assert file.read() == FILE_DATA
+        assert file.read() == FILE_DATA * 10
 
 
-class TestShredder:
+@pytest.mark.Win
+class TestShredderWindows:
+
+    def test_overwrite_file(self):
+        file = open(FILE_NAME, 'w+')
+        for _ in range(0, random.randint(0, 1) * 50):
+            file.write(str(random.randint(0, 1)) + " ")
 
     def test_delete_file(self):
         os.remove(FILE_PATH)
         assert not os.path.isfile(FILE_PATH)
 
+    @pytest.mark.V2
     def test_cipher_file(self):
-        # os.
-        'cipher /w:driveletter:\foldername'
+        if IS_WIN:
+            os.system(f'cipher /w:{PATH}')
         assert None
 
 
-class TestInspector:
+@pytest.mark.Linux
+class TestShredderLinux:
 
-    def test_check_file(self):
+    def test_shred_file(self):
+        os.system(f'shred -u {FILE_PATH}')
         assert None
