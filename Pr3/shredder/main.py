0911469f22bc6cde9675b438b8719acb43d1f29e
Vadim <vadimmm120@yandex.ru>
2021-10-04 13:32:19 +0300

    class TestCreator done

--- Pr3/shredder/main.py
+++ Pr3/shredder/main.py
@@ -1,15 +1,27 @@
-FILE_NAME = 'testfile'
-# FILE_PATH = ''
+import os
+
+PATH = os.path.dirname(__file__)
+FILE_NAME = 'testfile.txt'
+FILE_DATA = 'Credentials\n' \
+            'Email: somemail@gmail.com\n' \
+            'Password: secret\n' \
+            'Bank card: 13214564797\n' \
+            'CVV: 456'
 
 
 class TestCreator:
 
     def test_create_file(self):
-        file = open(FILE_NAME, 'rw')
-        assert None
+        file = open(FILE_NAME, 'w')
+        file.close()
+        assert os.path.isfile('')
 
     def test_fill_file(self):
-        assert None
+        file = open(FILE_NAME, 'w+')
+        file.write(FILE_DATA)
+        file.close()
+        file = open(FILE_NAME, 'r')
+        assert file.read() == FILE_DATA
 
 
 class TestShredder:
