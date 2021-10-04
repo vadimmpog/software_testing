0f2aaf8a86017d12117ad9d1c2462974452d2335
Vadim <vadimmm120@yandex.ru>
2021-10-04 13:08:23 +0300

    main.py created

---
+++ Pr3/shredder/main.py
@@ -0,0 +1,27 @@
+FILE_NAME = 'testfile'
+# FILE_PATH = ''
+
+
+class TestCreator:
+
+    def test_create_file(self):
+        file = open(FILE_NAME, 'rw')
+        assert None
+
+    def test_fill_file(self):
+        assert None
+
+
+class TestShredder:
+
+    def test_delete_file(self):
+        assert None
+
+    def test_cipher_file(self):
+        assert None
+
+
+class TestInspector:
+
+    def test_check_file(self):
+        assert None
